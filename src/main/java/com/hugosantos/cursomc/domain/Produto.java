bcf88bf7f61c55888933e40101d8cdbd05dc4f5f
Hugo Santos <hf.santos2332@gmail.com>
2022-10-21 16:27:15 -0300

    Cria a entidade Produto

---
+++ src/main/java/com/hugosantos/cursomc/domain/Produto.java
@@ -0,0 +1,98 @@
+package com.hugosantos.cursomc.domain;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Produto implements Serializable{
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    
+    private String nome;
+    private double preco;
+
+    @ManyToMany
+    @JoinTable( 
+        name = "PRODUTO_CATEGORIA",
+        joinColumns = @JoinColumn(name = "produto_id"),
+        inverseJoinColumns = @JoinColumn(name = "categoria_id")
+    )
+    private List<Categoria> categorias;
+
+    public Produto(){
+        this.categorias = new ArrayList<Categoria>();
+    }
+
+    public Produto(Integer id, String nome, double preco) {
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+        this.categorias = new ArrayList<Categoria>();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public double getPreco() {
+        return preco;
+    }
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+    public List<Categoria> getCategorias(){
+        ArrayList<Categoria> copiaCategorias = new ArrayList<Categoria>();
+        copiaCategorias.addAll(this.categorias);
+
+        return copiaCategorias;
+    }
+    public void adicionarCategoria(Categoria categoria){
+        if(categoria != null && !this.categorias.contains(categoria)) this.categorias.add(categoria);
+    }
+    public void adicionarCategorias(List<Categoria> categorias){
+        this.categorias.addAll(categorias);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.id);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Produto other = (Produto) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+}
