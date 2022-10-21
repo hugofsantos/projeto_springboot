3317bfacafb76e6bc9569b7a913797ed130c6eb9
Hugo Santos <hf.santos2332@gmail.com>
2022-10-21 15:39:55 -0300

    Cria operação de instanciação

--- src/main/java/com/hugosantos/cursomc/CursomcApplication.java
+++ src/main/java/com/hugosantos/cursomc/CursomcApplication.java
@@ -1,13 +1,29 @@
 package com.hugosantos.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.hugosantos.cursomc.domain.Categoria;
+import com.hugosantos.cursomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner{
+	@Autowired
+	CategoriaRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escrotório");
+		
+		repository.saveAll(Arrays.asList(cat1, cat2));
+	}
 }
