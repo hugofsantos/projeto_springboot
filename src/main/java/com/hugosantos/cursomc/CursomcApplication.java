25128ef33a721d74edaf3479bec280cd67cbe770
Hugo Santos <hf.santos2332@gmail.com>
2022-10-21 16:30:01 -0300

    Modifica main para inserir os produtos e seus relacionamentos com as categorias

--- src/main/java/com/hugosantos/cursomc/CursomcApplication.java
+++ src/main/java/com/hugosantos/cursomc/CursomcApplication.java
@@ -8,12 +8,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.hugosantos.cursomc.domain.Categoria;
+import com.hugosantos.cursomc.domain.Produto;
 import com.hugosantos.cursomc.repositories.CategoriaRepository;
+import com.hugosantos.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner{
 	@Autowired
-	CategoriaRepository repository;
+	CategoriaRepository categoriaRepository;
+
+	@Autowired
+	ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -23,7 +28,19 @@ public class CursomcApplication implements CommandLineRunner{
 	public void run(String... args) throws Exception {
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escrotório");
-		
-		repository.saveAll(Arrays.asList(cat1, cat2));
+
+		Produto p1 = new Produto(null, "Computador", 2000);
+		Produto p2 = new Produto(null, "Impressora", 800);
+		Produto p3 = new Produto(null, "Mouse", 80);
+
+		cat1.adicionarProdutos(Arrays.asList(p1, p2, p3));
+		cat2.adicionarProduto(p2);
+
+		p1.adicionarCategoria(cat1);
+		p2.adicionarCategorias(Arrays.asList(cat1, cat2));
+		p3.adicionarCategoria(cat1);
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 }
